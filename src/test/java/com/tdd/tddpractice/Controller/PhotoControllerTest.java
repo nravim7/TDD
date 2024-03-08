72658e9bd8e305781eae2f7c37d0208906b674c3
Ravi N <your.email@example.com>
2024-03-08 20:38:45 +0530

    TDD

---
+++ src/test/java/com/tdd/tddpractice/Controller/PhotoControllerTest.java
@@ -0,0 +1,27 @@
+package com.tdd.tddpractice.Controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+@AutoConfigureMockMvc
+public class PhotoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnPhotos() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/photos"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers
+                        .jsonPath("$", Matchers.hasSize(2)));
+    }
+}
